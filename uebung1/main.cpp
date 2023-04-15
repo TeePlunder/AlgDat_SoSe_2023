e8857b9afee5274660b684dbd3eba473b88255ce
TeePlunder <leon.bergmann23@gmail.com>
2023-04-15 15:53:39 +0200

    convert fahrenheit in celsius

--- uebung1/main.cpp
+++ uebung1/main.cpp
@@ -5,16 +5,25 @@ using namespace std;
 int main() {
     string tempUnit;
     const string celsiusUnit = "C", fahrenheitUnit = "F";
-    double tempValue;
+    double tempValue = 0.0, convertedTemp = 0.0;
 
     // set temperature unit
-    cout << "Möchten Sie C (Celsius) oder F (Fahrenheit) angeben? (" << celsiusUnit << "/" << fahrenheitUnit << "): ";
+    cout << "Möchten Sie die Temperatur " << celsiusUnit << " (Celsius) oder " << fahrenheitUnit
+         << " (Fahrenheit) angeben? (" << celsiusUnit << "/" << fahrenheitUnit << "): ";
     cin >> tempUnit;
-    if(tempUnit != "c" || tempUnit != "f") {
+    if (tempUnit != celsiusUnit && tempUnit != fahrenheitUnit) {
         throw exception();
     }
 
-    if(tempUnit == "c")
+    // set temperature value
+    cout << "Welche Temperatur soll umgewandet werden?: ";
+    cin >> tempValue;
+
+    if (tempUnit == fahrenheitUnit) {
+        cout << tempValue << "F wird in Celsius umgerechnet" << endl;
+        convertedTemp = 5.0 / 9.0 * (tempValue - 32.0);
+        cout << tempValue << fahrenheitUnit << " = " << convertedTemp << celsiusUnit << endl;
+    }
 
     return 0;
 }
