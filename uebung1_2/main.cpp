603bcba062856ae87e70399a9bbceecd47ebef32
TeePlunder <leon.bergmann23@gmail.com>
2023-04-21 09:14:35 +0200

    add decimal to binary convertion basic logic

--- uebung1_2/main.cpp
+++ uebung1_2/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cout << "decimal to binary - 13 -> ??";
+    int decimal = 13;
+    int binary [4];
+
+    int count = 0;
+    do {
+        int binaryNumber = decimal % 2;
+        binary[count] = binaryNumber;
+        decimal = decimal / 2;
+        count++;
+    } while (decimal > 0);
+
     return 0;
 }
