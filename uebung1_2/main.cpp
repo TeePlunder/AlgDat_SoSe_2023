28191316621e539d72bc86be25364c86d1aedb08
TeePlunder <leon.bergmann23@gmail.com>
2023-04-21 12:09:29 +0200

    convert a decimal to binary

--- uebung1_2/main.cpp
+++ uebung1_2/main.cpp
@@ -3,18 +3,32 @@
 using namespace std;
 
 int main() {
+    int decimal = 223;
+    int numberSystem = 2;
 
-    cout << "decimal to binary - 13 -> ??";
-    int decimal = 13;
-    int binary [4];
+    cout << decimal << " (10)";
+
+    // using array
+    // const char sym[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    // using pointer
+    const char *sym = "0123456789abcdef";
 
     int count = 0;
+    int rest[32];
     do {
-        int binaryNumber = decimal % 2;
-        binary[count] = binaryNumber;
-        decimal = decimal / 2;
+        rest[count] = decimal % numberSystem;
+        decimal = decimal / numberSystem;
         count++;
     } while (decimal > 0);
 
+    cout << " -> ";
+
+    for(int x = count - 1; x >= 0; x--) {
+        int restValue = rest[x];
+        char value = sym[restValue];
+        cout << value;
+    }
+
+    cout << " (" << numberSystem << ")" << endl;
     return 0;
 }
