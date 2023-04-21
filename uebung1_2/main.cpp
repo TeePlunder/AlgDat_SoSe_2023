87563a86c2b070d1ab089dd95a9983b5350fffbe
TeePlunder <leon.bergmann23@gmail.com>
2023-04-21 15:09:47 +0200

    add while loop that exists only when decimal is 0 and overall improvements

--- uebung1_2/main.cpp
+++ uebung1_2/main.cpp
@@ -21,35 +21,48 @@ int newConsoleInput(const string& message,int minRange, int maxRange) {
     }
     return inputValue;
 }
+
 int main() {
-    int decimal = 223;
-    int numberSystem = 2;
+    int decimal, numberSystem;
+    const int MIN_DECIMAL_RANGE = 0, MAX_DECIMAL_RANGE = INT_MAX;
+    const int MIN_NUMBER_SYSTEM = 2, MAX_NUMBER_SYSTEM=16;
+    bool terminateProgram = false;
 
-    cout << decimal << " (10)";
-
-    // using array
-    // const char sym[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    // using pointer
-    const char *sym = "0123456789abcdef";
-
-    int count = 0;
-    int rest[32];
-    do {
-        rest[count] = decimal % numberSystem;
-        decimal = decimal / numberSystem;
-        count++;
-    } while (decimal > 0);
-
-    cout << " -> ";
-
-    for(int x = count - 1; x >= 0; x--) {
-        int restValue = rest[x];
-        char value = sym[restValue];
-        cout << value;
+    while (!terminateProgram) {
+        cout << "neue Konvertierung gestartet" << endl;
         decimal = newConsoleInput("Welche Dezimalzahl soll umgewandelt werden? [0 = Programmende]", MIN_DECIMAL_RANGE, MAX_DECIMAL_RANGE);
+
+        if(decimal == 0) {
+            terminateProgram = true;
+        }
+
         numberSystem = newConsoleInput("In welches Zahlensystem soll die Zahl umgewandelt werden?", MIN_NUMBER_SYSTEM, MAX_NUMBER_SYSTEM);
+
+        cout << decimal << " (10) ";
+
+        // using array
+        // const char sym[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        // using pointer
+        const char *sym = "0123456789abcdef";
+
+        int count = 0;
+        int rest[32];
+        do {
+            rest[count] = decimal % numberSystem;
+            decimal = decimal / numberSystem;
+            count++;
+        } while (decimal > 0);
+
+        cout << "-> ";
+
+        for(int x = count - 1; x >= 0; x--) {
+            cout << sym[rest[x]];
+        }
+
+        cout << " (" << numberSystem << ")" << endl << endl;
     }
 
-    cout << " (" << numberSystem << ")" << endl;
+    cout << "Das Programm wird beendet";
     return 0;
 }
+
