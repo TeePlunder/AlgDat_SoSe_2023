c51e6d018b266eecc0d26f6e478bd683bac31b84
TeePlunder <leon.bergmann23@gmail.com>
2023-05-12 10:36:27 +0200

    input a file by console input and read it

--- uebung2_2/main.cpp
+++ uebung2_2/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    // std::ifstream
-    // std::iostream
+    string filename, article;
+    double preis;
+
+    cout << "Geben Sie den Dateinamen ein (ohne Dateiendung): ";
+    cin >> filename;
+
+    ifstream file("../" + filename + ".txt");
+
+    if(file.is_open()) {
+        while (file.good()) {
+            file >> article >> preis;
+            cout << article << ": " << preis << " €" << endl;
+        }
+        file.close();
+    }
+    else {
+        cout << "Fehler beim öffnen der Datei";
+    }
     return 0;
 }
