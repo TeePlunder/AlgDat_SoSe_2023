966b3a5b2799304216ac29c2ed4a5613185d8f49
TeePlunder <leon.bergmann23@gmail.com>
2023-06-02 15:21:58 +0200

    add insert method

--- uebung4/d_list.cpp
+++ uebung4/d_list.cpp
@@ -5,32 +5,41 @@
  *      Author: weigert
  */
 #include "list.h"
+
 using namespace std;
 
-D_LISTELEM::D_LISTELEM()
-{
-	ID=0;
-	next = 0;
-	prev = 0;
-}
-D_LISTE::D_LISTE()
-{
-	head = new D_LISTELEM;
-	tail = new D_LISTELEM;
-	head->prev = head;
-	head->next = tail;
-	tail->prev = head;
-	tail->next = tail;
+D_LISTELEM::D_LISTELEM() {
+    ID = 0;
+    next = 0;
+    prev = 0;
 }
 
-D_LISTE::~D_LISTE()
-{
-  ;
+D_LISTE::D_LISTE() {
+    head = new D_LISTELEM;
+    tail = new D_LISTELEM;
+    head->prev = head;
+    head->next = tail;
+    tail->prev = head;
+    tail->next = tail;
 }
 
-void D_LISTE::insert(int ID)
-{
-  ;
+D_LISTE::~D_LISTE() {
+    ;
+}
+
+void D_LISTE::insert(int ID) {
+    D_LISTELEM *current = head;
+    auto *neu = new D_LISTELEM();
+    neu->ID = ID;
+
+    while (current->next != current->next->next && current->next->ID < ID) {
+        current = current->next;
+    }
+
+    neu->next = current->next;
+    neu->prev = current;
+    current->next->prev = neu;
+    current->next = neu;
 }
 
 
@@ -47,7 +56,7 @@ int D_LISTE::printlist() {
     cout << endl;
     return count;
 }
-int D_LISTE::reverse_printlist()
-{
-  ;
+
+int D_LISTE::reverse_printlist() {
+    ;
 }
