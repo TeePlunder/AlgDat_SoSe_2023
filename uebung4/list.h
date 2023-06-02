9a53ebd2a963598123a2c6835a27ef178f5b7d4c
TeePlunder <leon.bergmann23@gmail.com>
2023-06-02 13:52:40 +0200

    format

--- uebung4/list.h
+++ uebung4/list.h
@@ -6,48 +6,59 @@
 
 using namespace std;
 
-class LISTELEM
-{
+class LISTELEM {
     friend class LISTE;
-private: int ID;
-         LISTELEM *next;
-public: LISTELEM();
+
+private:
+    int ID;
+    LISTELEM *next;
+public:
+    LISTELEM();
 };
 
-class LISTE
-{
-private: LISTELEM *head;
-	 LISTELEM *tail;
-public:	LISTE();
-        ~LISTE();
-        void append(int ID);  //Element ans Ende anhaengen
-        void insert(int ID);  //sortiertes Einfuegen
-        void remove(int ID); //Element entfernen
-        void connect(const LISTE &liste2);//Liste2 wird angegaengt
-        int printlist();     //Ausgabe aller Listenelemente
-};
+class LISTE {
+private:
+    LISTELEM *head;
+    LISTELEM *tail;
+public:
+    LISTE();
 
+    ~LISTE();
+
+    void append(int ID);  //Element ans Ende anhaengen
+    void insert(int ID);  //sortiertes Einfuegen
+    void remove(int ID); //Element entfernen
+    void connect(const LISTE &liste2);//Liste2 wird angegaengt
+    int printlist();     //Ausgabe aller Listenelemente
+};
 
 
 //////////////////////////////////////////////////////////////////
 //Listenelement einer doppelt verketteten Liste
-class D_LISTELEM
-{
-	friend class D_LISTE;
-private: int ID;
-        D_LISTELEM *next;
-        D_LISTELEM *prev;
-public: D_LISTELEM();
+class D_LISTELEM {
+    friend class D_LISTE;
+
+private:
+    int ID;
+    D_LISTELEM *next;
+    D_LISTELEM *prev;
+public:
+    D_LISTELEM();
 };
 
-class D_LISTE
-{
-private: D_LISTELEM *head;
-         D_LISTELEM *tail;
-public:	D_LISTE();
-        ~D_LISTE();
-        void insert(int ID);  //sortiertes Einfuegen
-        int printlist();
-        int reverse_printlist();//Elemente rueckwaerts ausgeben
+class D_LISTE {
+private:
+    D_LISTELEM *head;
+    D_LISTELEM *tail;
+public:
+    D_LISTE();
+
+    ~D_LISTE();
+
+    void insert(int ID);  //sortiertes Einfuegen
+    int printlist();
+
+    int reverse_printlist();//Elemente rueckwaerts ausgeben
 };
+
 #endif
