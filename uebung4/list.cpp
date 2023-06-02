5b97b591cc45f5fedc5692a980f8ff9a71e8cb46
TeePlunder <leon.bergmann23@gmail.com>
2023-06-02 10:07:36 +0200

    add remove methode

--- uebung4/list.cpp
+++ uebung4/list.cpp
@@ -1,60 +1,84 @@
 
 #include"list.h"
 
-LISTELEM::LISTELEM()
-{
-	ID=0;
-	next = 0;
+LISTELEM::LISTELEM() {
+    ID = 0;
+    next = 0;
 }
 
-LISTE::LISTE()
-{
-	head = new LISTELEM;
-	tail = new LISTELEM;
-	head->next = tail;
-	tail->next = tail;
-}
-LISTE::~LISTE()
-{
-;
-}
-void LISTE::append(int ID)
-{
-	LISTELEM *such, *neu;
-	
-	such = head;
-
-	neu = new LISTELEM;
-	neu-> ID = ID;
-
-	while(such->next!= such->next->next)
-	{
-		such = such->next;
-		count++;
-	}
-	such->next = neu;
-	neu->next = tail;
-
-	return;
+LISTE::LISTE() {
+    head = new LISTELEM;
+    tail = new LISTELEM;
+    head->next = tail;
+    tail->next = tail;
 }
 
-void LISTE::insert(int ID)
-{
-;
+LISTE::~LISTE() {
+    ;
 }
 
-void LISTE::connect(const LISTE &liste2)
-{
-;
-}
-void LISTE::remove(int ID)
-{
-;
+void LISTE::append(int ID) {
+    LISTELEM *such, *neu;
+
+    such = head;
+
+    neu = new LISTELEM;
+    neu->ID = ID;
+
+    while (such->next != such->next->next) {
+        such = such->next;
+        count++;
+    }
+    such->next = neu;
+    neu->next = tail;
+
+    return;
 }
 
-int LISTE::printlist()
-{
-;
+void LISTE::insert(int ID) {
+    LISTELEM *such, *neu;
+
+
+}
+
+void LISTE::connect(const LISTE &liste2) {
+    ;
+}
+
+void LISTE::remove(int ID) {
+    LISTELEM *current = head;
+    LISTELEM *prev = nullptr;
+
+    while (current != nullptr && current->ID != ID) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        cout << "Das Element " << ID << " Konnte nicht gefunden werden" << endl;
+        return;
+    }
+
+    // fist element in list
+    if (prev == nullptr) {
+        head = current->next;
+        if (current == tail) {
+            tail = nullptr;
+        }
+    } else {
+        prev->next = current->next;
+        if (current == tail) {
+            tail = prev;
+        }
+    }
+
+    delete current;
+    cout << "Element mit der ID " << ID << " wurde entfernt" << endl;
+
+}
+
+int LISTE::printlist() {
+    ;
 }
 
 
