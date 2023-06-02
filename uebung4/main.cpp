e0402df8a6693bbd589b65635d7873237e184c04
TeePlunder <leon.bergmann23@gmail.com>
2023-06-02 13:19:11 +0200

    add and remove

--- uebung4/main.cpp
+++ uebung4/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "list.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    LISTE list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(5);
+
+    list.insert(4);
+
+    list.printlist();
+
+    list.remove(1);
+    list.printlist();
+
     return 0;
 }
