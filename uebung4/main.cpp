acc123a578f744b617b6a4c2363869ba2e984679
TeePlunder <leon.bergmann23@gmail.com>
2023-06-03 15:56:28 +0200

    add dlist to main

--- uebung4/main.cpp
+++ uebung4/main.cpp
@@ -27,6 +27,22 @@ int main() {
 
     cout << "-----------------------" << endl;
     cout << "Doppelt verkettete Listen" << endl;
-
+    D_LISTE dlist;
+    dlist.insert(1);
+    dlist.insert(2);
+    dlist.insert(6);
+    dlist.insert(3);
+    dlist.printlist();
+    dlist.append(7);
+    dlist.append(5);
+    dlist.printlist();
+    dlist.remove(7);
+    dlist.printlist();
+    dlist.reverse_printlist();
+    D_LISTE l2;
+    l2.append(9);
+    l2.append(10);
+    dlist.connect(l2);
+    dlist.printlist();
     return 0;
 }
