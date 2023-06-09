dafed6070df0d885ce9299a680c89de6b687250d
TeePlunder <leon.bergmann23@gmail.com>
2023-06-09 10:45:33 +0200

    add predefined files

---
+++ uebung5/stack/stapel_imp.h
@@ -0,0 +1,43 @@
+
+#ifndef STAPEL_IMP_H_
+#define STAPEL_IMP_H_
+
+#include"stapel.h"
+
+
+template <typename ST>
+STAPEL<ST>::STAPEL()
+{
+	first = 0;
+	count=0;
+}
+template <typename ST>
+STAPEL<ST>::~STAPEL()
+{
+	;
+}
+template <typename ST>
+void STAPEL<ST>::push(ST d)
+{
+	;
+}
+
+template <typename ST>
+ST STAPEL<ST>::pop()
+{
+	;
+}
+
+template <typename ST>
+bool STAPEL<ST>::empty()
+{
+	;
+}
+template <typename ST>
+
+int STAPEL<ST>::number()
+{
+	;
+}
+
+#endif /* STAPEL_IMP_H_ */
