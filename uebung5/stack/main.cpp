25a1cef04eadcdff14b8bd74eaa584e07b994b5e
TeePlunder <leon.bergmann23@gmail.com>
2023-06-09 11:18:31 +0200

    implement some example usage

--- uebung5/stack/main.cpp
+++ uebung5/stack/main.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int main() {
     STAPEL<int> st;
 
+    cout << "Leer? " << st.empty() << endl;
+    st.pop();
     st.push(5);
+    st.push(4);
+    st.push(2);
+    st.push(1);
+    st.push(8);
+    st.push(9);
+    cout << "Anzahl: " << st.number() << endl;
+    cout << "Leer? " << st.empty() << endl;
+
+    st.pop();
+    st.pop();
+    st.pop();
+    cout << "Anzahl: " << st.number() << endl;
     return 0;
 }
